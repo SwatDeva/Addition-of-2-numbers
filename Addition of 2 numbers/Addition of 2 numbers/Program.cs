5f227e4cefff29869d74aa6d44dbfb4a53ab0b02
SwatDeva <shwethluvsall@gmail.com>
2014-12-25 10:51:01 +0000

    Initial Commit

---
+++ Addition of 2 numbers/Addition of 2 numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_of_2_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            
+            Console.WriteLine("The sum of number1 and number2 is " + program.addTwoNumbers(2, 3));
+        }
+
+
+        private int addTwoNumbers(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
